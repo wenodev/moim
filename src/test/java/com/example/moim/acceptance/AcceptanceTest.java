3705697cf09fc1f4abde5429459d106388046079
wenodev <logicalstark1105@gmail.com>
2022-07-10 15:56:07 +0900

    test: 주최자 인수테스트 작성

--- src/test/java/com/example/moim/acceptance/AcceptanceTest.java
+++ src/test/java/com/example/moim/acceptance/AcceptanceTest.java
@@ -1,11 +1,23 @@
 package com.example.moim.acceptance;
 
 import com.example.moim.acceptance.utils.DatabaseCleanup;
+import com.example.moim.domain.Gender;
+import com.example.moim.dto.HostRequest;
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.time.LocalDate;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
@@ -22,5 +34,26 @@ public class AcceptanceTest {
             databaseCleanup.afterPropertiesSet();
         }
         databaseCleanup.execute();
+    }
+
+    /**
+     * WHEN 주최자가 회원정보를 요청하면
+     * THEN 저장한다
+     */
+    @Test
+    void 주최자_회원_가입() {
+        HostRequest request = new HostRequest(
+                "주최자", LocalDate.now(), Gender.MALE, "host",
+                "password", "weno@next.com", "next"
+        );
+
+        ExtractableResponse<Response> response = given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post("/host")
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED);
     }
 }
