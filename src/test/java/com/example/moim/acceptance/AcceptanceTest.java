7006789b41db5449e5e8945463b724ba35b4b24a
wenodev <logicalstark1105@gmail.com>
2022-07-10 15:13:44 +0900

    test: 인수테스트 설정

---
+++ src/test/java/com/example/moim/acceptance/AcceptanceTest.java
@@ -0,0 +1,26 @@
+package com.example.moim.acceptance;
+
+import com.example.moim.acceptance.utils.DatabaseCleanup;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+            databaseCleanup.afterPropertiesSet();
+        }
+        databaseCleanup.execute();
+    }
+}
